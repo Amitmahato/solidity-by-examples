d681e6739ac1e7f4412092e85e85c10303e7a303
Amit Mahato <amit643.mahato@gmail.com>
2022-10-20 09:34:01 +0545

    core: add task   to deploy a given contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,23 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+task("deploy", "Deploy a contract to the network.")
+  .addParam("contract", "The name of the contract to deploy")
+  .addOptionalVariadicPositionalParam(
+    "rest",
+    "...rest of the optional parameters that are directly passed to the contract's constructor. Pass in the order that the constructor expects."
+  )
+  .setAction(async (args, hre) => {
+    // Compile all the contracts
+    await hre.run("compile");
+
+    const Contract = await hre.ethers.getContractFactory(args.contract);
+    const contract = await Contract.deploy(...(args.rest ? args.rest : []));
+
+    await contract.deployed();
+
+    console.log(`${args.contract} deployed to ${contract.address}`);
+  });
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
